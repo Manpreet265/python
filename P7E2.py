0856d8e64abec58940594b95bc6922c92ff4f94d
Manpreet kaur <mkaur2@cifpfbmoll.eu>
2019-11-05 17:20:04 +0100

    segunda subida

---
+++ P7E2.py
@@ -0,0 +1,10 @@
+"""Escribe un programa que lea el nombre y los dos apellidos de una persona (en tres cadenas de caracteres diferentes), los pase como parámetros a una función, y ésta debe unirlos y devolver una única cadena. La cadena final la imprimirá el programa por pantalla."""
+def learnombreappelidos(nombre,apellido_1,apellido_2):
+    return(nombre,apellido_1,apellido_2)
+nombre=input("Escribe su nombre")
+print(nombre)
+apellido_1=input("Escribe su primer apellido")
+print(apellido_1)
+apellido_2=input("Escribe su segundo apellido")
+print(apellido_2)
+leernombreapellidos=print(nombre,apellido_1,apellido_2)
